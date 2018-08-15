7070391eb97418a80e706476c744150090b26411
Jeremy Zheng <jitang.zheng@gmail.com>
2018-08-15 13:42:53 -0700

    feat: dashboard demo page

---
+++ dashboard/src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React, {
+  Component
+} from 'react'
+import {
+  Spin
+} from 'antd'
+
+class Widget extends Component {
+  render() {
+    return (<Spin size="large"/>)
+  }
+}
+
+export default Widget
