7e1093258a5477a949ff507f0b842c07fb9f481b
Jeremy Zheng <jitang.zheng@gmail.com>
2018-08-22 22:09:00 -0700

    fix: use env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // extern crate log4rs;
+extern crate env_logger;
 extern crate lotus;
 
 #[macro_use]
@@ -7,8 +8,10 @@ extern crate log;
 fn main() {
     let run = || {
         // log4rs::init_file("log4rs.yml", Default::default())?;
+        env_logger::init();
         lotus::app::main()
     };
+
     if let Err(e) = run() {
         error!("{:?}", e);
     }
