f177eeff1cb5f1b6a8a11e8bb7b77269b1fe0a62
Jeremy Zheng <jitang.zheng@gmail.com>
2018-09-18 14:01:07 -0700

    chore: add port range

--- docker/rustup.sh
+++ docker/rustup.sh
@@ -8,3 +8,6 @@ source $HOME/.zshrc
 rustup override set nightly
 rustup component add rls-preview rust-analysis rust-src rustfmt-preview
 cargo install --force diesel_cli
+
+# https://github.com/rust-lang/rust/issues/50504#issuecomment-410550021
+echo 'export RUSTFLAGS="-Aproc-macro-derive-resolution-fallback"' >> $HOME/.zshrc
