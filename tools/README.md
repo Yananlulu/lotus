832cb1cc70d25a2f3c43d67b2ceacd35ccd72247
Jeremy Zheng <jitang.zheng@gmail.com>
2018-11-03 11:09:12 -0700

    refactor: remove tools

--- tools/README.md
+++
@@ -1,8 +0,0 @@
-# README
-
-```bash
-bundle install
-rake -T # list all tasks
-rake db:migrate # migrate database to latest schema
-rake db:rollback # rollback database to previous schema
-```
--- tools/db/migrate/20180815191431_add_created_at_to_schema_migrations.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatedAtToSchemaMigrations < ActiveRecord::Migration[5.2]
-  def change
-    add_column :schema_migrations, :created_at, :datetime, null: false, default: -> { 'CURRENT_TIMESTAMP' }
-  end
-end
