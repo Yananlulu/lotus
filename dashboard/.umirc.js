cc565d6ae92f126fd3362c5f7a14b79e637a6b7f
Jeremy Zheng <jitang.zheng@gmail.com>
2018-11-24 11:18:19 -0800

    feat(dashboard): add packages

--- dashboard/.umirc.js
+++ dashboard/.umirc.js
@@ -1,9 +1,20 @@
 export default {
+  base: '/my',
   plugins: [
     [
       'umi-plugin-react', {
         dva: {
           immer: true
+        },
+        antd: true,
+        dynamicImport: {
+          webpackChunkName: true,
+          loadingComponent: 'components/Loading'
+        },
+        locale: {
+          default: 'zh-CN',
+          baseNavigator: true,
+          antd: true
         }
       }
     ]
