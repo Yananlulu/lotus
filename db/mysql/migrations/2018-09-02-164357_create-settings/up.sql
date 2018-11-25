22850ec19097f987c349c91663db1c9c512e99f1
Jeremy Zheng <jitang.zheng@gmail.com>
2018-11-24 19:06:19 -0800

    refactor: remove uneeded files

--- db/mysql/migrations/2018-09-02-164357_create-settings/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE settings(
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  `key` VARCHAR(255) NOT NULL,
-  value BLOB NOT NULL,
-  updated_at DATETIME NOT NULL,
-  created_at DATETIME NOT NULL
-);
-
-CREATE UNIQUE INDEX idx_settings_key ON settings(`key`);
--- db/postgresql/migrations/2018-09-02-164237_create-settings/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE settings(
-  id BIGSERIAL PRIMARY KEY,
-  key VARCHAR(255) NOT NULL,
-  value BYTEA NOT NULL,
-  updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
-);
-
-CREATE UNIQUE INDEX idx_settings_key ON settings(key);
